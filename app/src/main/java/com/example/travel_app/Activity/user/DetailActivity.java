81b8b705da31ddc4f31e363d0ca13c842fe66b33
Dias203 <diasduong@gmail.com>
2024-11-25 17:27:08 +0700

    first commit

---
+++ app/src/main/java/com/example/travel_app/Activity/user/DetailActivity.java
@@ -0,0 +1,56 @@
+package com.example.travel_app.Activity.user;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.bumptech.glide.Glide;
+import com.example.travel_app.Activity.BaseActivity;
+import com.example.travel_app.Domain.ItemDomain;
+import com.example.travel_app.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends BaseActivity {
+    ActivityDetailBinding binding;
+    private ItemDomain object;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+        getIntentExtra();
+        setVariable();
+    }
+
+    private void setVariable() {
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$"+object.getPrice());
+        binding.backBtn.setOnClickListener(view -> finish());
+        binding.bedTxt.setText(""+object.getBed());
+        binding.addressTxt.setText(object.getAddress());
+        binding.durationTxt.setText(object.getDuration());
+        binding.distanceTxt.setText(object.getDistance());
+        binding.descriptionTxt.setText(object.getDescription());
+        //binding.ratingTxt.setText(object.getScore()+" Rating");
+        binding.ratingBar.setRating((float)object.getScore());
+
+        Glide.with(DetailActivity.this)
+                .load(object.getPic())
+                .into(binding.pic);
+
+        Log.i("Image Link", object.getTitle() + " " + object.getPic());
+
+        binding.bookTour.setOnClickListener(view -> {
+            Intent intent  = new Intent(DetailActivity.this, TicketActivity.class);
+            intent.putExtra("object", object);
+            startActivity(intent);
+        });
+    }
+
+    private void getIntentExtra() {
+        object = (ItemDomain) getIntent().getSerializableExtra("object");
+    }
+}
